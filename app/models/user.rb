a5924f64b39a078e3c7ebab5f45218a5e1bdb145
Imran Hossain <blur-imran@hotmail.com>
2024-05-04 05:52:14 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+         
+  validates :password, length: { minimum: 1 }
 end
