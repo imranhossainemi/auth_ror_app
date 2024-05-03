c8f8465449daa48d48b6865a319947a482a41989
Imran Hossain <blur-imran@hotmail.com>
2024-05-03 10:41:10 +0600

    Auth error fix with devise and database

--- db/migrate/20240429043207_add_details_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :last_login_time, :datetime
-    add_column :users, :registration_time, :datetime
-    add_column :users, :status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_29_043207) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.datetime "last_login_time"
-    t.datetime "registration_time"
     t.string "status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
