914376a89ff7d71c9af7404d4606dd5859c385ed
Imran Hossain <blur-imran@hotmail.com>
2024-05-04 05:52:08 +0600

    Update admin_controller.rb

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,29 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:block, :unblock, :destroy]
 
   def index
     @users = User.all
+  end
+  
+  def block
+    @user.update(blocked: true)
+    redirect_to admin_index_path
+  end
+
+  def unblock
+    @user.update(blocked: false)
+    redirect_to admin_index_path
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_index_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
