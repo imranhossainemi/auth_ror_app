9fbf857e38e4f7bbe3fbb65f627d6825667f482d
Imran Hossain <blur-imran@hotmail.com>
2024-05-15 03:46:04 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   get 'admin/index'
   devise_for :users
-
+  post 'admin/ban/:id', to: 'admin#ban_user', as: 'admin_ban_user'
+  post 'admin/unban/:id', to: 'admin#unban_user', as: 'admin_unban_user'
 
   # Defines the root path route ("/")
   root "admin#index"
   
   resources :admin, only: [:index] do
     member do
-      put :block
-      put :unblock
+      post :ban_user
+      post :unban_user
       delete :destroy
     end
   end
