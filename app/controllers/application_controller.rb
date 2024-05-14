8481a20d9457dba464a6cdd22ccdf35ea6b722ac
Imran Hossain <blur-imran@hotmail.com>
2024-05-15 03:45:41 +0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :check_user_status
-
-  private
-
-  def check_user_status
-    return unless current_user && (current_user.rebuff? || !current_user.persisted?)
-
-    sign_out current_user
-    redirect_to new_user_session_path
-  end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fullname rebuff])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[fullname rebuff])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[fullname])
   end
 end
